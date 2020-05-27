99c0c520612763631cdcb8b550970790ab16d077
Riku <kukkaniemi.riku@gmail.com>
2020-05-27 22:04:05 +0300

    Add stub for introduction

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <v-app>
     <NavBar />
-    <v-content>
-      <h1>Testi</h1>
+    <v-content class="mx-auto mt-12">
+      <v-card class="mx-auto mt-12" outlined>
+      <Introduction />
+      </v-card>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import NavBar from "./components/NavBar";
+import Introduction from "./components/Introduction";
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    Introduction,
   }
 };
 </script>
