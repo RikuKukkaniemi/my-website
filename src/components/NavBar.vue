04bae56ff365371331215f8214441825c85f564f
Riku <kukkaniemi.riku@gmail.com>
2020-05-27 22:03:10 +0300

    Remove unnecessary div

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <v-app-bar absolute elevate-on-scroll scroll-target="#scrolling-techniques-7">
-      <v-tabs centered>
-        <v-tab>Suomeksi</v-tab>
-        <v-tab>In English</v-tab>
-      </v-tabs>
-    </v-app-bar>
-  </div>
+  <v-app-bar absolute elevate-on-scroll scroll-target="#scrolling-techniques-7">
+    <v-tabs centered>
+      <v-tab>Suomeksi</v-tab>
+      <v-tab>In English</v-tab>
+    </v-tabs>
+  </v-app-bar>
 </template>
