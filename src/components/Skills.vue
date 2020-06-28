98403e08837472240dd072ecc93f3b3eb37f64c3
Riku <kukkaniemi.riku@gmail.com>
2020-06-28 23:49:02 +0300

    Add skeleton for skills component

---
+++ src/components/Skills.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2 class="text-center my-3">Osaaminen</h2>
+    <v-card class="mx-auto" max-width="344">
+      <v-treeview dense open-all :items="items"></v-treeview>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        name: "Backend :",
+        children: [
+          { id: 2, name: "Laravel" },
+          { id: 3, name: "PHP" },
+        ]
+      },
+    ]
+  })
+};
+</script>
