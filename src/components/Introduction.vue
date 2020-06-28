d9e2a30158e32acec6e1753e5f7b5ee92040eb0d
Riku <kukkaniemi.riku@gmail.com>
2020-06-28 23:48:46 +0300

    Remove card layout, add headers

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,30 +1,6 @@
 <template>
-  <v-card
-    class="mx-12 my-12"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
-
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <v-btn text>Share</v-btn>
-
-      <v-btn
-        color="purple"
-        text
-      >
-        Explore
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div class="text-center">
+    <h1>Moi, nimeni on Riku.</h1>
+    <h2>Olen sovelluskehittäjä.</h2>
+  </div>
 </template>
