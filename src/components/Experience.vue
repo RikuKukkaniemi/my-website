d382344f24dbfb767b57f0b65cc7e2256acc55a2
Riku <kukkaniemi.riku@gmail.com>
2020-07-03 23:39:53 +0300

    Improve style on mobile, add description texts

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h2 class="text-center my-3">Työkokemus</h2>
-    <v-timeline>
+    <v-timeline :dense="$vuetify.breakpoint.smAndDown">
       <v-timeline-item v-for="(job, j) in jobs" :key="j" :color="job.color">
         <template v-slot:opposite>
-          <span :class="`headline font-weight-bold ${job.color}--text`" v-text="job.year"></span>
+          <span :class="`headline font-weight-bold ${job.color}--text`" v-text="job.time"></span>
         </template>
         <v-card class="elevation-2">
-          <v-card-title class="headline">{{ job.title }}</v-card-title>
-          <v-card-text>{{ job.description }}</v-card-text>
+          <v-card-title :class="`headline font-weight-bold ${job.color}--text`">{{ job.title }}</v-card-title>
+          <v-card-subtitle v-if="$vuetify.breakpoint.smAndDown">{{ job.time }}</v-card-subtitle>
+          <v-card-text>
+            <p v-for="(paragraph, p) in job.description" :key="p">
+              {{ paragraph }}
+            </p>
+            <p v-if="job.link">{{ job.link.description }}<a :href="job.link.href">{{ job.link.href }}</a></p>
+            </v-card-text>
         </v-card>
       </v-timeline-item>
     </v-timeline>
@@ -21,17 +27,24 @@ export default {
     jobs: [
       {
         color: "pink",
-        year: "01/2019-",
-        title: "Paytrail, sovelluskehittäjä",
-        description:
-          "Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae."
+        time: "01/2019-",
+        title: "Paytrail",
+        description: [
+          "Työskentelyä sovelluskehittäjänä Suomen kattavinta maksupalvelua tarjoavassa yrityksessä. Työnkuvaan kuuluu backend painotteista sovelluskehitystä ja -sunnittelua 4-5 hengen tiimeissä.",
+        ],
       },
       {
         color: "blue",
-        year: "01/2018-12/2018",
-        title: "Jyväskylän yliopisto, tutkimusavustaja",
-        description:
-          "Terveydenhuoltoon liittyvien prototyyppien ohjelmointi. Prototyypeissä pyritty hyödyntämään tekoälyä käyttäen pääosin IBM Watson työkaluja."
+        time: "01/2018-12/2018",
+        title: "Jyväskylän yliopisto",
+        description: [
+          "Työskentelyä tutkimusavustajana projektissa, jossa selvitettiin, miten tekoälyä voitaisiin hyödyntää terveydenhuollossa. Työnkuvaan kuului tekoälyprototyyppien suunnittelua ja ohjelmointia 3-5 hengen tiimeissä.",
+          'Projektin tuloksista kirjoitettiin tieteellinen julkaisu "Tekoälypohjaisten teknologioiden testaus prototyyppisovelluksilla". Julkaisussa kerrotaan projektissa käytetyistä tekoälyteknologoista ja esitellään toteutettuja prototyyppejä.',
+        ],
+        link: {
+          description: "Linkki julkaisuun: ",
+          href: "https://jyx.jyu.fi/handle/123456789/63938"
+        }
       }
     ]
   })
