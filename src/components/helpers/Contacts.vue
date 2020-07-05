4d5e44273419e90c68453b0d001abf5c686f3a5d
Riku <kukkaniemi.riku@gmail.com>
2020-07-05 15:14:28 +0300

    Add helper for contact icons

---
+++ src/components/helpers/Contacts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-btn
+      v-for="(contact, c) in contacts"
+      :key="c"
+      class="mx-4 white--text"
+      icon
+      :href="contact.href"
+    >
+      <v-icon size="24px">{{ contact.icon }}</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    contacts: [
+      {
+        icon: "mdi-github",
+        href: "https://github.com/RikuKukkaniemi"
+      },
+      {
+        icon: "mdi-linkedin",
+        href: "https://www.linkedin.com/in/riku-kukkaniemi-7a3942167/"
+      }
+    ]
+  })
+};
+</script>
