e925b233cf2c25a687dafffd003dc45e6aa697be
Riku <kukkaniemi.riku@gmail.com>
2020-08-12 21:48:08 +0300

    Add transparency to card

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,28 +1,15 @@
 <template>
-  <v-card shaped max-width="700" class="mx-auto text-center">
+  <v-card elevation="0" color="transparent" class="text-center" max-width="700">
     <v-card-subtitle class="headline text--primary">Moi, nimeni on Riku.</v-card-subtitle>
-    <v-avatar size="62">
-      <img
-        src="../assets/profile.jpg"
-        alt="Riku"
-      >
+    <v-avatar size="128">
+      <img src="../assets/profile.jpg" alt="Riku" />
     </v-avatar>
     <v-card-subtitle class="title text--primary">Olen full-stack -kehittäjä.</v-card-subtitle>
     <v-card-text>
       <div class="text--primary">Olen työskennellut sovelluskehittäjänä vuodesta 2018 lähtien.</div>
       <div
         class="text--primary mt-2"
-      >Valmistuin Jyväskylän yliopistosta filosofian maisteriksi vuonna 2020. Opiskelin pääaineena tietotekniikkaa.</div>
+      >Valmistuin vuonna 2020 filosofian maisteriksi Jyväskylän yliopistosta, jossa opiskelin pääaineena tietotekniikkaa.</div>
     </v-card-text>
   </v-card>
 </template>
-
-<script>
-/* import ContactDetails from "./ContactDetails"; */
-
-export default {
-  components: {
-/*     ContactDetails, */
-  },
-};
-</script>
