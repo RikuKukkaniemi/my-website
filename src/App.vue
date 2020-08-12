5cc8c3fdc3b119e6489fdc969e117b8113362b1f
Riku <kukkaniemi.riku@gmail.com>
2020-08-12 21:49:34 +0300

    Add navigation buttons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
   <v-app>
     <NavBar />
-    <v-main class="mx-md-10">
-      <Introduction class="my-6" />
-      <v-divider />
-      <Skills class="my-6" />
-      <v-divider />
-      <Experience class="my-6" />
-      <v-divider />
-      <Education class="my-6" />
-    </v-main>
+    <v-content class="mx-md-10">
+      <section id="introduction">
+        <Introduction class="mx-auto" />
+        <v-row align="center" class="mx-auto" justify="center">
+          <DownButton target="#skills" />
+        </v-row>
+      </section>
+
+      <v-divider class="my-6" />
+
+      <section id="skills">
+        <v-row align="center" class="mx-auto" justify="center">
+          <UpButton target="#introduction" />
+        </v-row>
+        <Skills class="my-6" />
+        <v-row align="center" class="mx-auto" justify="center">
+          <DownButton target="#experience" />
+        </v-row>
+      </section>
+
+      <v-divider class="my-6" />
+
+      <section id="experience">
+        <v-row align="center" class="mx-auto" justify="center">
+          <UpButton target="#skills" />
+        </v-row>
+        <Experience class="my-6" />
+        <v-row align="center" class="mx-auto" justify="center">
+          <DownButton target="#education" />
+        </v-row>
+      </section>
+
+      <v-divider class="my-6" />
+
+      <section id="education">
+        <v-row align="center" class="mx-auto" justify="center">
+          <UpButton target="#experience" />
+        </v-row>
+        <Education class="my-6" />
+      </section>
+    </v-content>
     <BottomNavBar />
   </v-app>
 </template>
@@ -21,6 +53,8 @@ import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
 import BottomNavBar from "./components/BottomNavBar";
+import DownButton from "./components/utility/DownButton";
+import UpButton from "./components/utility/UpButton";
 
 export default {
   components: {
@@ -30,6 +64,8 @@ export default {
     Experience,
     Education,
     BottomNavBar,
-  }
+    DownButton,
+    UpButton,
+  },
 };
 </script>
---
+++ src/components/utility/UpButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn class="align-self-end" fab outlined @click="$vuetify.goTo(target)">
+    <v-icon>mdi-chevron-double-up</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+      target: String,
+  },
+};
+</script>
