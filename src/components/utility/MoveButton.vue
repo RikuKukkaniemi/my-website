35346be5de7b71f8caa369147c22973190fcf152
Riku <kukkaniemi.riku@gmail.com>
2020-09-03 20:20:23 +0300

    Add MoveButton

---
+++ src/components/utility/MoveButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-btn rounded @click="$vuetify.goTo(target)">{{ text }}</v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+      text: String,
+      target: String,
+  },
+};
+</script>
